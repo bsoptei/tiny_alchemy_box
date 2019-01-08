893e5fba928acfdb69a5504794925fbef1348e70
bsoptei <bsoptei@gmail.com>
2019-01-08 20:12:46 +0100

    make drastic changes in the GUI

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ import('./tiny_alchemy_box')
     .then(wasm => {
         const renderButton = document.getElementById('render');
         const helpButton = document.getElementById('help');
-        const closeHelpButton = document.getElementById('close_help');
-        const helpText = document.getElementById('help_text');
         const scriptBox = document.getElementById('tab_script');
         const positionIndicator = document.getElementById('cursor_position');
 
         renderButton.addEventListener('click', () => {
-            wasm.process(scriptBox.value);
+            const inputText = scriptBox.value;
+            if (inputText.length > 0) {
+                wasm.process(scriptBox.value);
+            }
         });
 
         helpButton.addEventListener('click', () => {
-            helpText.hidden = false;
-        });
-
-        closeHelpButton.addEventListener('click', () => {
-            helpText.hidden = true;
+            window.open('', '_blank');
         });
 
         const boxEffects = ['keydown', 'keyup', 'keypress', 'click', 'mouseover', 'mousedown'];
